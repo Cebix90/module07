079c9bc41f6654edf4c6582e0535adf8dfe245b8
Cebix90 <cebulmat@gmail.com>
2024-03-31 15:33:33 +0200

    added tests for updateBookTitle, updateBookGenre, updateBookNumberOfPages, updateBookAuthor

--- src/test/java/pl/cebix/LibraryDAOTest.java
+++ src/test/java/pl/cebix/LibraryDAOTest.java
@@ -233,4 +233,215 @@ public class LibraryDAOTest {
             System.setOut(originalOut);
         }
     }
+
+    @Nested
+    class TestUpdateBookTitle {
+        @Test
+        public void testIfNewTitleHasValidData() {
+            Book book = new Book("Title", "Fantasy", 250);
+
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            libraryDAO.updateBookTitle(book.getTitle(), "New Title");
+
+            verify(session).merge(book);
+            verify(transaction).commit();
+        }
+
+        @Test
+        public void testIfNewTitleIsNull() {
+            String expectedMessageIfBooksTitleIsNull = "Book's title cannot be null or empty.";
+
+            testUpdateBookTitleWhenThrowsExceptionAndReturnAMessage(null, expectedMessageIfBooksTitleIsNull);
+        }
+
+        @Test
+        public void testIfNewTitleIsEmpty() {
+            String expectedMessageIfBooksTitleIsEmpty = "Book's title cannot be null or empty.";
+
+            testUpdateBookTitleWhenThrowsExceptionAndReturnAMessage("", expectedMessageIfBooksTitleIsEmpty);
+        }
+
+        private void testUpdateBookTitleWhenThrowsExceptionAndReturnAMessage(String newTitle, String expectedMessage) {
+            Book book = new Book("Title", "Fantasy", 250);
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            PrintStream originalOut = System.out;
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            libraryDAO.updateBookTitle(book.getTitle(), newTitle);
+
+            assertTrue(outContent.toString().contains(expectedMessage));
+
+            System.setOut(originalOut);
+        }
+    }
+
+    @Nested
+    class TestUpdateBookGenre {
+        @Test
+        public void testIfNewGenreHasValidData() {
+            Book book = new Book("Title", "Fantasy", 250);
+
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            libraryDAO.updateBookGenre(book.getTitle(), "New Genre");
+
+            verify(session).merge(book);
+            verify(transaction).commit();
+        }
+
+        @Test
+        public void testIfNewGenreIsNull() {
+            String expectedMessageIfBooksGenreIsNull = "Book's genre cannot be null or empty.";
+
+            testUpdateBookGenreWhenThrowsExceptionAndReturnAMessage(null, expectedMessageIfBooksGenreIsNull);
+        }
+
+        @Test
+        public void testIfNewGenreIsEmpty() {
+            String expectedMessageIfBooksGenreIsEmpty = "Book's genre cannot be null or empty.";
+
+            testUpdateBookGenreWhenThrowsExceptionAndReturnAMessage("", expectedMessageIfBooksGenreIsEmpty);
+        }
+
+        private void testUpdateBookGenreWhenThrowsExceptionAndReturnAMessage(String newGenre, String expectedMessage) {
+            Book book = new Book("Title", "Fantasy", 250);
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            PrintStream originalOut = System.out;
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            libraryDAO.updateBookGenre(book.getTitle(), newGenre);
+
+            assertTrue(outContent.toString().contains(expectedMessage));
+
+            System.setOut(originalOut);
+        }
+    }
+
+    @Nested
+    class TestUpdateBookNumberOfPages {
+        @Test
+        public void testWithValidData() {
+            Book book = new Book("Title", "Fantasy", 250);
+
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            libraryDAO.updateBookNumberOfPages(book.getTitle(), 260);
+
+            verify(session).merge(book);
+            verify(transaction).commit();
+        }
+
+        @Test
+        public void testIfNewNumberOfPagesIsNull() {
+            String expectedMessageIfBooksGenreIsNull = "Book's number of pages must be a positive number between 1 and 3000.";
+
+            testUpdateBookNumberOfPagesWhenThrowsExceptionAndReturnAMessage(null, expectedMessageIfBooksGenreIsNull);
+        }
+
+        @Test
+        public void testIfNewNumberOfPagesIsBelowTheLimit() {
+            String expectedMessageIfBooksGenreIsEmpty = "Book's number of pages must be a positive number between 1 and 3000.";
+
+            testUpdateBookNumberOfPagesWhenThrowsExceptionAndReturnAMessage(0, expectedMessageIfBooksGenreIsEmpty);
+        }
+
+        @Test
+        public void testIfNewNumberOfPagesIsAboveTheLimit() {
+            String expectedMessageIfBooksGenreIsEmpty = "Book's number of pages must be a positive number between 1 and 3000.";
+
+            testUpdateBookNumberOfPagesWhenThrowsExceptionAndReturnAMessage(3001, expectedMessageIfBooksGenreIsEmpty);
+        }
+
+        private void testUpdateBookNumberOfPagesWhenThrowsExceptionAndReturnAMessage(Integer newNumberOfPages, String expectedMessage) {
+            Book book = new Book("Title", "Fantasy", 250);
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            PrintStream originalOut = System.out;
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            libraryDAO.updateBookNumberOfPages(book.getTitle(), newNumberOfPages);
+
+            assertTrue(outContent.toString().contains(expectedMessage));
+
+            System.setOut(originalOut);
+        }
+    }
+
+    @Nested
+    class TestUpdateAuthor {
+        @Test
+        public void testIfNewAuthorHasValidData() {
+            Author newAuthor = new Author("New Author", 30, "Genre");
+            Book book = new Book("Title", "Fantasy", 250);
+
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            libraryDAO.updateBookAuthor(book.getTitle(), newAuthor);
+
+            verify(session).merge(book);
+            verify(transaction).commit();
+        }
+
+        @Test
+        public void testIfNewAuthorIsNull() {
+            Book book = new Book("Title", "Fantasy", 250);
+            Session session = mock(Session.class);
+            Transaction transaction = mock(Transaction.class);
+
+            when(sessionFactory.openSession()).thenReturn(session);
+            when(session.beginTransaction()).thenReturn(transaction);
+            doReturn(book).when(libraryDAO).findBookByTitle(book.getTitle());
+
+            PrintStream originalOut = System.out;
+            ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContent));
+
+            libraryDAO.updateBookAuthor(book.getTitle(), null);
+
+            assertTrue(outContent.toString().contains("Book's author cannot be null."));
+
+            System.setOut(originalOut);
+        }
+    }
 }
